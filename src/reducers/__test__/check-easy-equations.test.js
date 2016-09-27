c2837514c8cdb6179c15c25b6bfa2eb2800f89dc
Lance <lance@lancesnider.com>
2016-09-27 11:44:22 -0600

    Fix  Error

--- src/reducers/__test__/check-easy-equations.test.js
+++ src/reducers/__test__/check-easy-equations.test.js
@@ -3,11 +3,12 @@ import T from 'i18n-react'
 import Texts from '../../texts.js'
 
 it('checks for easy equations', () => {
-  expect(checkEasyEquation([22, "="], [true, false])).toEqual(T.texts.feedback.tooShort),
-  expect(checkEasyEquation([22, "*", 2, "="], [true, false])).toEqual(""),
-  expect(checkEasyEquation([22, "*", 0], [true, false, true])).toEqual(T.texts.feedback.zero),
+  expect(checkEasyEquation([2, 2, "="], [true, true, false])).toEqual(T.texts.feedback.tooShort),
+  expect(checkEasyEquation([2, 2, "*", 2, "="], [true, true, false])).toEqual(""),
+  expect(checkEasyEquation([2, 2, "*", 0], [true, true, false, true])).toEqual(T.texts.feedback.zero),
   expect(checkEasyEquation([0, "/"], [true, false])).toEqual(T.texts.feedback.zero),
-  expect(checkEasyEquation([22, "^", 2, "="], [true, false, true, false])).toEqual(""),
-  expect(checkEasyEquation([4, "+", 0, "*", 300], [true, false, true, false, true])).toEqual(T.texts.feedback.zero),
+  expect(checkEasyEquation([2, 2, "^", 2, "="], [true, true, false, true, false])).toEqual(""),
+  expect(checkEasyEquation([4, "+", 0, "*", 300], [true, false, true, false, true, true, true])).toEqual(T.texts.feedback.zero),
   expect(checkEasyEquation([0], [true])).toEqual(T.texts.feedback.zero)
+  expect(checkEasyEquation([5, 6, 5, "*", 3, "="], [true, true, true, false, true, false])).toEqual("")
 })
